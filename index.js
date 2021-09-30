bcee5e415d01d3fef6f28b0127b482213a72bab4
deje0014 <72152264+deje0014@users.noreply.github.com>
2021-09-30 15:07:04 -0400

    Created Cards style

--- index.js
+++ index.js
@@ -9,27 +9,15 @@ const APP = {
   listeners: ()=>{
     document.getElementById('search-form').addEventListener('submit', APP.getAction);
     document.getElementById('get-my-location').addEventListener('click', APP.getLocation);
-    document.getElementById('toggle').addEventListener('click', APP.checkFrequency)
-    // document.getElementById('search-form').addEventListener('change', APP.autocomplete);
+    // document.getElementById('toggle').addEventListener('click', APP.checkFrequency)
     APP.handleStorage();
   },
-  //   autocomplete: ()=>{
-  //     let SEARCH_STRING = document.getElementById('search-field').value;
-  //     console.log(SEARCH_STRING)
-  //     let url = `https://api.locationiq.com/v1/autocomplete.php?key=pk.2b1a67e9f2ac89606bf8cd1672855f63&q=${SEARCH_STRING}`
-  //     fetch(url)
-  //     .then(response => response.json())
-  //     .then(data => console.log(data))
-
-  // },
   findCity: async (location)=> {
     let reverse = await getCity(location);
-    let {city, state, country} = reverse.address;
-    let text = `${city}, ${state}, ${country}`;
-    let div = document.getElementById('location');
-    div.classList.remove('hidden');
+    let {city, country} = reverse.address;
+    let text = `${city}, ${country}`;
     let page = document.getElementById('city');
-    page.append(text);
+    page.textContent = text;
   },
   getAction: async (ev)=>{
     ev.preventDefault();
@@ -63,8 +51,8 @@ const APP = {
       try {
         console.log(location);
         const forecast = await getForecast(location)
+        APP.mainCard(forecast);
         APP.makeHourlyCards(forecast);
-        APP.makeDailyCards(forecast);
         APP.addStorage(forecast, forecast.current.dt);
         console.log(forecast);
       } catch (error) {
@@ -82,8 +70,8 @@ const APP = {
     let storage = localStorage.getItem('weather-app');
     if (storage) {
       let obj = JSON.parse(storage);
-      APP.makeHourlyCards(obj.coord);
-      APP.makeDailyCards(obj.coord);
+      APP.mainCard(obj.coord);
+      // APP.makeDailyCards(obj.coord);
       APP.findCity(obj.coord);
       let timeNow = Date.now();
       if ((timeNow - obj.timestamp) * 1000 > 3600) {
@@ -94,167 +82,83 @@ const APP = {
       }
     }
   },
-  makeHourlyCards: (forecast)=>{
-    let frequencyToggle = document.getElementById('frequency');
-    frequencyToggle.classList.remove('hidden');
+  mainCard: (forecast)=>{
+    // let frequencyToggle = document.getElementById('frequency');
+    // frequencyToggle.classList.remove('hidden');
     let {humidity, temp, wind_speed, weather, feels_like, dt} = forecast.current;
 
-    let {min, max } = forecast.daily[0].temp;
+    let {min, max, pop } = forecast.daily[0].temp;
+    let {sunrise, sunset} = forecast.daily[0];
     
-    let page = document.getElementById('weather-hourly');
-    page.innerHTML = '';
-    let df = document.createElement('div');
-    df.innerHTML = `
-    <div class="container mx-auto bg-white border rounded flex flex-col justify-center items-center text-center py-5 w-64 sm:w-80 shadow-lg cursor-pointer pb-5">
-      <h2 class="font-bold text-lg">NOW</h2>
-      <div class='weather-img p-2 flex flex-col'>
-        <img src="https://openweathermap.org/img/w/${weather['0'].icon}.png" alt="${weather['0'].description}">
-        <p class='description'>${weather['0'].main}</p>
-      </div>
-      <div class="temp-now pb-1">
-        <h3 class="font-bold text-3xl pb-3">${Math.round(temp)}º</h3>
-        <p>Feels like ${Math.round(feels_like)}º</p>
-      </div>
-      <div class="extra-weather-info flex gap-10 py-2">
-        <p>Wind: ${Math.round(wind_speed)}km/h</p>
-        <p>Humidity: ${humidity}%</p>
-      </div>
-      <div class="temp-day pl-10 flex gap-5">
-      <h4 class='font-bold'>Today</h4>
-      <div class='flex pl-5 gap-5'>
-      <p class="text-xl pb-3">${Math.round(min)}º</p>
-      <p class="text-xl pb-3"> ${Math.round(max)}º</p>
-      </div>
-      </div>
-      </div>`;
+/**
+ * Create / select main card components
+ */
 
-    /*
-    * Hourly forecast
-    */
+let tempNow = document.getElementById('tempNow');
+let weatherState = document.getElementById('weatherState');
+let tempMin = document.getElementById('temp-min');
+let tempMax = document.getElementById('temp-max');
+let precipitation = document.getElementById('precipitation');
+let humi = document.getElementById('humidity');
+let wind = document.getElementById('wind');
+let feels = document.getElementById('feels-like');
+let sun = document.getElementById('sunrise');
+let night = document.getElementById('sunset');
 
-    let hourly = forecast.hourly.slice(0, 6);
-    let div = document.createElement('div');
+/**
+ * get info to update components
+ */
+  tempNow.textContent = `${Math.round(temp)}º`;
+  tempMin.textContent = `${Math.round(min)}º`;
+  tempMax.textContent = `${Math.round(max)}º`;
+  wind.textContent = `${Math.round(wind_speed)} km/h`;
+  humi.textContent = `${Math.round(humidity)}%`;
+  precipitation.textContent = `${pop}%`
+  feels.textContent = `${Math.round(feels_like)}º`;
+  weatherState.textContent = weather[0].main;
+
+  function getHours(time){
+    let date = new Date(time * 1000);
+    let hour = date.getHours();
+    let amPm = hour >= 12 ? ' pm' : ' am';
+    hour = (hour % 12) || 12;
+    let minutes = "0" + date.getMinutes();
+    return hour + ':' + minutes.toString().slice(-2) + amPm;
+  }
+
+  sun.textContent = getHours(sunrise);
+  night.textContent = getHours(sunset);
+
+  },
+  makeHourlyCards: (forecast)=>{
+    let hourlyCards = document.getElementById('hourly-cards');
     let frag = document.createDocumentFragment();
-    hourly.forEach(item => {
-      // let localTime = (forecast.timezone_offset <= 0 ? dt + forecast.timezone_offset : dt - forecast.timezone_offset) * 1000;
+    /**
+     * Hourly
+     */
+     let hourly = forecast.hourly.slice(1, 7);
+
+     hourly.forEach(item =>{
       let date = new Date(item.dt * 1000);
       let hour = date.getHours();
       let amPm = hour >= 12 ? 'pm' : 'am';
       hour = (hour % 12) || 12;
       let div = document.createElement('div');
-      div.classList.add('container', 'mx-auto', 'bg-white', 'border', 'rounded', 'flex', 'flex-col', 'justify-center','items-center', 'text-center', 'p-4', 'w-64', 'shadow-lg', 'cursor-pointe')
-        div.innerHTML = `
-        <h2 class="font-bold text-lg">${hour} ${amPm}</h2>
-        <div class='weather-img p-2 flex flex-col'>
-          <img src="https://openweathermap.org/img/w/${item.weather['0'].icon}.png" alt="${item.weather['0'].description}">
-          <p class='description'>${item.weather['0'].description}</p>
-        </div>
-        <div class="temp">
-          <h3 class="font-bold text-3xl pb-3">${Math.round(item.temp)}º</h3>
-          <p>Feels like ${Math.round(item.feels_like)}º</p>
-        </div>
-        <div class="extra-weather-info flex gap-10 py-2">
-          <p>Wind: ${Math.round(item.wind_speed)}km/h</p>
-          <p>Humidity: ${item.humidity}%</p>
-        </div>
-        `;
+      div.classList.add('weather-card-detail', 'w-16', 'h-28', 'text-center', 
+      'flex', 'flex-col', 'justify-between', 'p-1', 'm-2', 'rounded-xl', 'bg-black');
+      div.innerHTML = `
+      <p class="text-white">${hour} ${amPm}</p>
+      <img src="https://openweathermap.org/img/w/${item.weather['0'].icon}.png" alt="${item.weather['0'].description}">
+      <p class="text-white">${Math.round(item.temp)}º</p>
+      `;
       frag.append(div)
-    })
-    div.append(frag)
-    df.classList.add('pb-5', 'container');
-    div.classList.add('container', 'flex', 'flex-row', 'flex-wrap', 'gap-1', 'xl:grid', 'xl:grid-cols-6')
-    page.append(df, div);
+     })
+     hourlyCards.append(frag)
   },
-  makeDailyCards: (forecast)=>{
-    
-    function getDate(timestamp, sun){
-      let days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-      let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-      let date = new Date(timestamp * 1000);
-      let year = date.getFullYear();
-      let hour = date.getHours();
-      let dayOfTheWeek = days[date.getDay()];
-      let day = date.getDate();
-      let month = months[date.getMonth()];
-      let minutes = "0" + date.getMinutes();
-      if (sun == 'sun'){
-        let amPm = hour >= 12 ? 'pm' : 'am';
-        hour = (hour % 12) || 12;
-        return hour + ':' + minutes.toString().slice(-2) + amPm;
-      } else if (sun == 'week') {
-        return dayOfTheWeek;
-      }
-        else {
-      return  month + ' ' + year + ', ' + day}
-    }
-
-    let time = getDate(forecast.current.dt);
-    
-    let days = forecast.daily;
-    let today = days.slice(0,1)[0];
-    let page = document.getElementById('weather-daily');
-    page.innerHTML = '';
-    let df = document.createElement('div');
-    df.innerHTML = `
-    <div class="container mx-auto bg-white border rounded flex flex-col justify-center items-center text-center p-2 sm:p-2 w-64 sm:w-80 shadow-lg cursor-pointer">
-      <h2 class="font-bold text-lg">TODAY</h2>
-      <h3>${time}</h3>
-      <div class='weather-img p-2 flex flex-col'>
-        <img src="https://openweathermap.org/img/w/${today.weather[0].icon}.png" alt="${today.weather[0].description}">
-        <p class='description'>${today.weather[0].description}</p>
-      </div>
-      <div class="temp">
-        <h3 class="font-bold text-3xl pb-3">${Math.round(today.temp['min'])}º / ${Math.round(today.temp['max'])}</h3>
-        <span class='flex flex-row gap-4 sm:gap-7'>
-        <p>Sunrise: ${getDate(today.sunrise,'sun')}</p>
-        <p>Sunset: ${getDate(today.sunset,'sun')} </p>
-        </span>
-      </div>
-      <div class="extra-weather-info flex gap-10 py-2">
-        <p>Wind: ${Math.round(today.wind_speed)}km/h</p>
-        <p>Humidity: ${today.humidity}%</p>
-      </div>
-      </div>`;
-    
-      page.append(df);
-
-    // /*
-    // * Daily forecast
-    // */
-    let daily = days.slice(1, 7);
-    console.log(daily);
-    let div = document.createElement('div');
-    let frag = document.createDocumentFragment();
-    daily.forEach((item, index) => {
-      let day = getDate(item.dt, 'week');
-      let div = document.createElement('div');
-      div.classList.add('container', 'mx-auto', 'bg-white', 'border', 'rounded', 'flex', 'flex-col', 'justify-center','items-center', 'text-center', 'p-4', 'w-64', 'shadow-lg', 'cursor-pointe')
-        div.innerHTML = `
-        <h2 class="font-bold text-lg">${index == 0 ? 'Tomorrow' : day}</h2>
-        <div class='weather-img p-2 flex flex-col'>
-          <img src="https://openweathermap.org/img/w/${item.weather[0].icon}.png" class='w-24' alt="${item.weather[0].description}">
-          <p class='description'>${item.weather[0].description}</p>
-        </div>
-        <div class="temp">
-        <h3 class="font-bold text-3xl pb-3">${Math.round(item.temp['min'])}º / ${Math.round(item.temp['max'])}</h3>
-        <span class='flex flex-row gap-4'>
-        <p>Sunrise: ${getDate(item.sunrise,'sun')}</p>
-        <p>Sunset: ${getDate(item.sunset,'sun')} </p>
-        </span>
-      </div>
-      <div class="extra-weather-info flex gap-10 py-2">
-      <i class="fa-solid fa-wind"></i>
-        <p>Wind: ${Math.round(item.wind_speed)}km/h</p>
-        <p>Humidity: ${item.humidity}%</p>
-      </div>
-        </div>
-        `;
-      frag.append(div)
-    })
-    div.append(frag);
-    div.classList.add('container', 'flex', 'flex-row', 'flex-wrap', 'gap-1', 'xl:grid', 'xl:grid-cols-6');
-    df.classList.add('pb-2', 'sm:pm-5', 'container');
-    page.append(df, div);
+  makeDailyCards: (forecast) => {
+    let day = document.getElementById('day');
+    let date = document.getElementById('date');
+    let dailyCards = document.getElementById('daily-cards');
   },
   checkFrequency:()=>{
     let hourly = document.getElementById('weather-hourly')
@@ -274,4 +178,4 @@ const APP = {
   },
 }
 
-// document.addEventListener('DOMContentLoaded', APP.init)
+document.addEventListener('DOMContentLoaded', APP.init)
